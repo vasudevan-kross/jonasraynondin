25d90cad1ad2506d3c501d1ade8faf72b53ef870
vasudevan-kross <127483935+vasudevan-kross@users.noreply.github.com>
2026-01-28 09:55:12 +0530

    updated scroll

--- components/ScrollWrapper.js
+++ components/ScrollWrapper.js
@@ -3,7 +3,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function ScrollWrapper({ children }) {
     return (
-        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+        <ReactLenis root options={{ lerp: 0.1, smoothWheel: true, normalizeWheel: true }}>
             {children}
         </ReactLenis>
     );
